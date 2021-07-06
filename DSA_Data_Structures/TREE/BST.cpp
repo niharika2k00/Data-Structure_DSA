d4e4cf0c5a6d671dd136e3263295680a410387a4
niharika2k00 <dniharika16@gmail.com>
2021-07-06 22:52:56 +0530

    bst

--- DSA_Data_Structures/TREE/BST.cpp
+++ DSA_Data_Structures/TREE/BST.cpp
@@ -2,9 +2,125 @@
 #include <iostream>
 #include <vector>
 #include <bits/stdc++.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 using namespace std;
 
-int main()
+struct BST_Node
 {
+    int data;
+    struct BST_Node *right;
+    struct BST_Node *left;
+};
+
+struct BST_Node *start = NULL, *node = NULL; // Global variables are declared here...
+
+int main(void)
+{
+    int option;
+    do
+    {
+        printf("\n \n **************** CHOOSE AN OPTION **********************");
+        printf("\n \n 1. Create a Binary_Search_Tree.");
+        printf("\n 2. Display a Binary_Search_Tree.");
+        printf("\n 3. Add a node from the beginning.");
+        printf("\n 4. Add a node from the END.");
+        printf("\n 5. Add a node BEFORE a given node.");
+        printf("\n 6. Add a node AFTER a given node.");
+        printf("\n 7. Delete a node from the beginning.");
+        printf("\n 8. Delete a node from the END.");
+        printf("\n 9. Delete any given NODE.");
+        printf("\n 10. Delete the entire Binary_Search_Tree.");
+        printf("\n 11. MAXIMUM node value in the whole Binary_Search_Tree.  ");
+        printf("\n 12. REVERSE the Binary_Search_Tree. ");
+        printf("\n 13. Search an element in the Binary_Search_Tree.");
+        printf("\n 14. Delete ALL ODD elements.");
+        printf("\n 15. Sum of all elements.");
+        printf("\n 16. Insert element at a given POSITION. ");
+        printf("\n 17. Delete element at a given POSITION. ");
+        printf("\n 18. Print the REVERSE list. \n");
+        printf("\n ********************** EXIT *****************************");
+        printf("\n \n Choose a no. -> ");
+        scanf("%d", &option);
+
+        switch (option)
+        {
+        case 1:
+            insert();
+            break;
+        // case 2:
+        //     display(start);
+        //     // display();
+        //     break;
+        // case 3:
+        //     addbeginning();
+        //     break;
+        // case 4:
+        //     addend();
+        //     break;
+        // case 5:
+        //     addbefr_givennode();
+        //     break;
+        // case 6:
+        //     addaftr_givennode();
+        //     break;
+        // case 7:
+        //     delbeginning();
+        //     break;
+        // case 8:
+        //     delend();
+        //     break;
+        // case 9:
+        //     delany_givennode();
+        //     break;
+        // case 10:
+        //     delentire_linkedlist();
+        //     break;
+        // case 11:
+        //     maxNode();
+        //     break;
+        // case 12:
+        //     // Reverse();
+        //     Reverse(start);
+        //     break;
+        // case 13:
+        //     search(start);
+        //     break;
+        // case 14:
+        //     delodd_ele();
+        //     break;
+        // case 15:
+        //     findsum();
+        //     break;
+        // case 16:
+        //     insertele_position();
+        //     break;
+        // case 17:
+        //     delele_position();
+        //     break;
+
+        // case 18:
+        //     reversePrint(start);
+        //     break;
+        default:
+            printf("CHOOSE THE OPTION MENTIONED IN THE MENU.....");
+            break;
+        }
+
+    } while ((option >= 1) && (option <= 20));
+    return 0;
+}
+
+// Creation of Binary_Search_Tree.
+void insert()
+{
+    create();
+}
+
+void create()
+{
+    int data;
+    cout << "Enter the DATA  of the node to be inserted ";
+    cin >> data;
 }
